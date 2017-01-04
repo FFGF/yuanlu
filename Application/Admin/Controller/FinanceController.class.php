3131bb56c1b1e63042eb57091ca19c2feb70e2cc
FengGuoFei <m18818217017@163.com>
2017-01-04 21:03:01 +0800

    汇率录入修改
    汇率录入修改

--- Application/Admin/Controller/FinanceController.class.php
+++ Application/Admin/Controller/FinanceController.class.php
@@ -19,9 +19,35 @@ class FinanceController extends ChannelsController{
         $this->assign("historyExchangeRate",$historyExchangeRate);
         $this->display("Finance/inputExchangeRate");
     }
+    //判断某一天汇率是否输入
+    public function judgeExchangeRateInput(){
+        $date = I("date");
+        $flag = D("ExchangeRate")->judgeExchangeRateInput($date);
+        $json['code'] = 0;
+        $flag&&$json['code'] = 1;//数据已经存储了
+        $this->ajaxReturn($json);
+    }
     //保存前台输入的汇率
     public function saveExchangeRate(){
-        dump(I('post.'));
-        die();
+        $data = I("get.");
+        $result = D("ExchangeRate")->saveExchangeRate($data);
+        $json['code'] = 0;
+        if($result){$json['code'] = 1;}
+        $this->ajaxReturn($json);
+    }
+    //汇率修改
+    public function modifyExchangeRate(){
+        $timestamp = I("effect_date");
+        $date = date("Y-m-d",$timestamp);
+        $this->assign("date",$date);
+        $this->display("Finance/modifyExchangeRate");
+    }
+    //保存修改的数据
+    public function modifyExchangeRateData(){
+        $data = I("get.");
+        $result = D("ExchangeRate")->modifyExchangeRateData($data);
+        $json['code'] = 0;
+        if($result){$json['code'] = 1;}
+        $this->ajaxReturn($json);
     }
 }
--- Application/Admin/Model/ExchangeRateModel.class.php
+++ Application/Admin/Model/ExchangeRateModel.class.php
@@ -14,4 +14,24 @@ class ExchangeRateModel extends Model{
     public function getHistoryExchageRate(){
         return $this->order('effect_date desc')->select();
     }
+    //判断某一天是否输入数据
+    public function judgeExchangeRateInput($date){
+        $maps['effect_date'] = $date;
+        $result = $this->where($maps)->select();
+        $flag = true;
+        is_null($result)&&$flag = false;
+        return $flag;
+    }
+    //保存数据
+    public function saveExchangeRate($data){
+        return $this->add($data);
+    }
+    //修改数据
+    public function modifyExchangeRateData($data){
+        $maps['effect_date'] = $data['effect_date'];
+        $result = $this->where($maps)->save($data);
+        $flag = true;
+        is_null($result)&&$flag = false;
+        return $flag;
+    }
 }
